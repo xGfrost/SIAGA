02ddce5194441d027432208ae50acde30f5520cb
xGfrost <fadelakbar528@gmail.com>
2025-07-09 18:58:53 +0700

    SIAGA

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('send-wa', function(){
+    $response = Http::withHeaders([
+        'Authorization' => 'Ym1nx6mH6AXQBLT9yCJ1',
+    ])->post('https://api.fonnte.com/send', [
+        'target' => '6285755842855',
+        'message' => 'Waspada banjir',
+    ]);
+
+    dd(json_decode($response, true));
+});
