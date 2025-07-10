5deea2f79eec8820d3d0c93b33b45bb08cb8b499
xGfrost <fadelakbar528@gmail.com>
2025-07-10 14:35:23 +0700

    FIX : Routes API

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 use App\Http\Controllers\Api\MeasurementController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('measurements')->group(function () {
-    Route::post('/', [MeasurementController::class, 'store']);       // Tambah data
-});
+Route::apiResource('measurements', MeasurementController::class);
 
 
