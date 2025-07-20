ad22e4a3fc8f458e1233f919f6ffd91cdaa529cd
xGfrost <fadelakbar528@gmail.com>
2025-07-20 18:06:07 +0700

    UPDATE: Change the message notification

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('send-wa', function(){
         'Authorization' => 'Ym1nx6mH6AXQBLT9yCJ1',
     ])->post('https://api.fonnte.com/send', [
         'target' => '6281233503740',
-        'message' => 'Waspada banjir',
+        'message' => 'Warning! Ketinggian air telah mencappai 10 cm di RT 5/RW 6 Kelurahan Bendul Merisi.',
     ]);
 
     dd(json_decode($response, true));
