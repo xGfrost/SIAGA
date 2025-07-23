7fcca473c96f0d9552393e25f29ae16f36ee100d
xGfrost <fadelakbar528@gmail.com>
2025-07-24 02:32:38 +0700

    UPDATE: Notification the water level if get >= 10 cm it will send the notification

--- app/Http/Controllers/Api/MeasurementController.php
+++ app/Http/Controllers/Api/MeasurementController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Measurement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MeasurementController extends Controller
 {
@@ -14,17 +15,27 @@ class MeasurementController extends Controller
     }
 
     public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'water_level_cm' => 'numeric',
-            'rainfall_mm' => 'numeric',
+{
+    $validated = $request->validate([
+        'water_level_cm' => 'numeric',
+        'rainfall_mm' => 'numeric',
+    ]);
+
+    $measurement = Measurement::create($validated);
+
+    if ($measurement->water_level_cm >= 10) {
+        $response = Http::withHeaders([
+            'Authorization' => 'Ym1nx6mH6AXQBLT9yCJ1',
+        ])->post('https://api.fonnte.com/send', [
+            'target' => '6281233503740',
+            'message' => 'Warning! Ketinggian air telah mencapai ' . $measurement->water_level_cm . ' cm di RT 5/RW 6 Kelurahan Bendul Merisi.',
         ]);
-
-        $measurement = Measurement::create($validated);
-
-        return response()->json([
-            'message' => 'Measurement created successfully',
-            'data' => $measurement
-        ], 201);
     }
+
+    return response()->json([
+        'message' => 'Measurement created successfully',
+        'data' => $measurement
+    ], 201);
+}
+
 }
